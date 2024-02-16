a0cc1f813da405794f356ba8da1b73703f5df6e5
tatamamayo <iniautok@gmail.com>
2024-02-16 17:40:08 +0700

    Horizontal sudah aman

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -12,29 +12,28 @@ class LoginPage extends StatelessWidget {
         padding: EdgeInsets.only(top: 30),
         width: double.infinity,
         decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            colors: [
-              Colors.orange.shade900,
-              Colors.orange.shade800,
-              Colors.orange.shade400,
-            ]
-          )
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+            gradient: LinearGradient(begin: Alignment.topCenter, colors: [
+          Colors.orange.shade900,
+          Colors.orange.shade800,
+          Colors.orange.shade400,
+        ])),
+        child: ListView(
           children: <Widget>[
-            SizedBox(height: 80,),
+            SizedBox(
+              height: 80,
+            ),
             Padding(
               padding: EdgeInsets.all(20),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Text(
-                      'Sign In',
+                    'Sign In',
                     style: TextStyle(color: Colors.white, fontSize: 40),
                   ),
-                  SizedBox(height: 10,),
+                  SizedBox(
+                    height: 10,
+                  ),
                   Text(
                     'Welcome Back',
                     style: TextStyle(color: Colors.white, fontSize: 18),
@@ -42,108 +41,127 @@ class LoginPage extends StatelessWidget {
                 ],
               ),
             ),
-            SizedBox(height: 20,),
+            SizedBox(
+              height: 20,
+            ),
             Expanded(
-              child: SingleChildScrollView(
-                child: Container(
-                  decoration: BoxDecoration(
+              child: Container(
+                decoration: BoxDecoration(
                     color: Colors.white,
-                    borderRadius: BorderRadius.all(Radius.circular(60))
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.all(30),
-                    child: Column(
-                      children: <Widget>[
-                        SizedBox(height: 60,),
-                        Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(20),
-                            boxShadow: [BoxShadow(
-                              color: Color.fromRGBO(225, 95, 27, 0.3),
-                              blurRadius: 20,
-                              offset: Offset(0, 10)
-                            )],
-                          ),
-                          child: Column(
-                            children: <Widget>[
-                              Container(
-                                padding: EdgeInsets.all(10),
-                                decoration: BoxDecoration(
-                                  border: Border(bottom: BorderSide(color: Colors.grey))
-                                ),
-                                child: TextField(
-                                  decoration: InputDecoration(
-                                    hintText: 'Email or Phone Number',
-                                    hintStyle: TextStyle(color: Colors.grey),
-                                    border: InputBorder.none
-                                  ),
-                                ),
-                              ),
-                              Container(
-                                padding: EdgeInsets.all(10),
-                                child: TextField(
-                                  decoration: InputDecoration(
-                                      hintText: 'Password',
-                                      hintStyle: TextStyle(color: Colors.grey),
-                                      border: InputBorder.none
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                        SizedBox(height: 40,),
-                        TextButton(
-                          child: const Text(
-                            'Forgot Password ?', style: TextStyle(color: Colors.grey, fontSize: 15),
-                          ),
-                          onPressed: (){
-                            // Isi nanti!
-                          },
-                        ),
-                        SizedBox(height: 20,),
-                        Column(
-                          children: <Widget>[
-                            ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.orange.shade900,
-                                minimumSize: Size(350, 50)
-                              ),
-                              child: Text('Sign In', style: TextStyle(color: Colors.white, fontSize: 17, fontWeight: FontWeight.bold),),
-                              onPressed: (){
-                                Navigator.of(context).push(
-                                  MaterialPageRoute(builder: (context){
-                                    return MyStateFul(text: 'Welcome Back !');
-                                    },
-                                  ),
-                                );
-                              },
-                            ),
-                            SizedBox(height: 10,),
-                            OutlinedButton(
-                              style: OutlinedButton.styleFrom(
-                                side: BorderSide(color: Colors.orange.shade900),
-                                minimumSize: Size(350, 50)
-                              ),
-                              child: Text('Sign Up', style: TextStyle(color: Colors.orange.shade900, fontSize: 17, fontWeight: FontWeight.bold),),
-                              onPressed: () {
-                                Navigator.of(context).push(
-                                  MaterialPageRoute(builder: (context){
-                                      return RegisterPage();
-                                    },
-                                  ),
-                                );
-                              },
-                            ),
+                    borderRadius: BorderRadius.all(Radius.circular(60))),
+                child: Padding(
+                  padding: EdgeInsets.all(30),
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(
+                        height: 60,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Color.fromRGBO(225, 95, 27, 0.3),
+                                blurRadius: 20,
+                                offset: Offset(0, 10))
                           ],
                         ),
-                      ],
-                    ),
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                  border: Border(
+                                      bottom: BorderSide(color: Colors.grey))),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    hintText: 'Email or Phone Number',
+                                    hintStyle: TextStyle(color: Colors.grey),
+                                    border: InputBorder.none),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    hintText: 'Password',
+                                    hintStyle: TextStyle(color: Colors.grey),
+                                    border: InputBorder.none),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          // Isi nanti!
+                        },
+                        child: const Text(
+                          'Forgot Password ?',
+                          style: TextStyle(color: Colors.grey, fontSize: 15),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Column(
+                        children: <Widget>[
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) {
+                                    return MyStateFul(text: 'Welcome Back !');
+                                  },
+                                ),
+                              );
+                            },
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.orange.shade900,
+                                minimumSize: Size(350, 50)),
+                            child: Text(
+                              'Sign In',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          OutlinedButton(
+                            onPressed: () {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) {
+                                    return RegisterPage();
+                                  },
+                                ),
+                              );
+                            },
+                            style: OutlinedButton.styleFrom(
+                                side: BorderSide(color: Colors.orange.shade900),
+                                minimumSize: Size(350, 50)),
+                            child: Text(
+                              'Sign Up',
+                              style: TextStyle(
+                                  color: Colors.orange.shade900,
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
